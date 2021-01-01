74a6565a26fb9967bfba91e04dd5ab84e7df1984
Sumit Kumar <sumit.kumar@zensar.com>
2021-01-01 23:20:13 +0530

    sparated app into sub component and added react router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import { Route } from 'react-router-dom'
+
 import Homepage from './pages/homepage.component'
+
+
+function Hats(props) {
+  console.log(props);
+  return(
+    <div>Hats</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      {/* <Homepage /> */}
+      <Route exact path='/' component={Homepage} ></Route>
+      <Route exact path='/shop/hats' component={Hats} ></Route>
     </div>
   );
 }
