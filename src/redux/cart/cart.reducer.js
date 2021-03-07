f880054036333ee6197027280d3984d21ecd0f0a
Sumit Kumar <sumit.kumar@zensar.com>
2021-03-07 20:07:52 +0530

    added cart reducer

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,20 @@
+import { CartActionTypes } from "./cart.types";
+
+const INIT_STATE = {
+    hidden: true
+}
+
+const cartReducer = (state = INIT_STATE, action) => {
+    switch (action.type) {
+        case CartActionTypes.TOOGLE_CART_HIDDEN:
+            return {
+                ...state,
+                hidden: !state.hidden
+            };
+    
+        default:
+            return state;
+    }
+}
+
+export default cartReducer
